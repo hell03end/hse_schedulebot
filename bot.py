135752039981b8664711dae6a6004fed1c7b631f
Bogdan <evstrat.bg@mail.ru>
2017-10-12 22:14:01 +0300

    add example func

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ import sys
 from telegram.ext import Updater
 from telegram import Bot
 
-from config import sentry_handler, TOKENS
+from config import TOKENS
 
 
-bot = Bot(TOKENS['KIMBERBOT'])
+bot = Bot(TOKENS['TEST'])
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -22,31 +22,10 @@ if __name__ == '__main__':
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                             level=logging.DEBUG)
 
+    from service import common_handlers
+
     dp = updater.dispatcher
+    common_handlers.register(dp)
 
-
-    make_pause.register(dp)
-    make_unpause.register(dp)
-    delete_client.register(dp)
-    stop_task.register(dp)
-    all_tasks.register(dp)
-    start_planned_task.register(dp)
-    start_task.register(dp)
-    reports.register(dp)
-    add_company.register(dp)
-    planned_tasks.register(dp)
-    sign_out.register(dp)
-    edit_planned_tasks.register(dp)
-
-    admin.register(dp)
-
-    search_docs.register(dp)
-    add_doc.register(dp)
-
-    make_act.register(dp)
-    make_invoice.register(dp)
-
-    handle_inline.register(dp)
-    common.register(dp)
     updater.start_polling()
     updater.idle()
---
+++ service/common_handlers.py
@@ -0,0 +1,22 @@
+import emoji
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler
+
+from utils.keyboards import start_keyboard
+from models import Users
+from logger import log
+
+
+@log
+def start(bot, update):
+    uid = update.message.from_user.id
+    winking_face = emoji.emojize(':winking_face:')
+    msg = f'Привет! У меня можно подсмотреть расписания ' \
+          f'электричек или твоей учебы{winking_face}'
+    bot.send_message(uid,
+                     msg,
+                     reply_markup=ReplyKeyboardMarkup(start_keyboard))
+
+
+def register(dp):
+    dp.add_handler(CommandHandler('start', start))
