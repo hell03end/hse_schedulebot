00bdcfa585e6c201e1ebb836661c2b2e48361372
Alex <eshobee@gmail.com>
2017-10-15 01:41:24 +0300

    (alpha) realize update_db

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .update_db import (api, get_emails, fetch_schedules, format_schedule,
+                        format_schedules, update_schedules)
+from .schema import MESSAGE_SCHEMA, POST_SCHEMA
