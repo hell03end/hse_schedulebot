bd4f4f5aafcf4fa3dd28f74a40bda937f143012f
Alex <eshobee@gmail.com>
2017-10-15 19:40:38 +0300

    add _logger property to wrapped function, impove imports, add annotations

--- logger.py
+++ logger.py
@@ -1,17 +1,19 @@
 import logging
-import functools
+from collections import Callable
+from functools import wraps
 
 
-def log(func):
+def log(func: Callable) -> Callable:
     logger = logging.getLogger(func.__module__)
 
-    @functools.wraps(func)
-    def decorator(*args, **kwargs):
-        logger.info('Entering: %s', func.__name__)
-        for a in args:
-            logger.info(a)
+    @wraps(func)
+    def decorator(*args, **kwargs) -> object:
+        logger.info("Entering: {}".format(func.__name__))
+        for arg in args:
+            logger.info(arg)
+        func._logger = logger
         result = func(*args, **kwargs)
-        logger.info('Exiting: %s', func.__name__)
+        logger.info("Exiting: {}".format(func.__name__))
         return result
 
     return decorator
