7216097dc26462d5042f1760718e0d27f50d3167
Alex <eshobee@gmail.com>
2017-10-15 23:22:33 +0300

    for schedule module add register function to pass dispatcher to all submodules

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -1,1 +1,6 @@
-from .week_schedule import register
+def register(dispatcher: object) -> None:
+    from .week_schedule import register as week_schedule_reg
+    from .eve_schedule import register as eve_schedule_reg
+
+    week_schedule_reg(dispatcher)
+    eve_schedule_reg(dispatcher)
