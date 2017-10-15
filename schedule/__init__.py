4c6fb6016947d17ace832a34bbd7b85a4bc4ce27
Alex <eshobee@gmail.com>
2017-10-15 22:11:56 +0300

    add schedule draft

---
+++ schedule/__init__.py
@@ -0,0 +1,1 @@
+from .week_schedule import register
---
+++ schedule/week_schedule.py
@@ -0,0 +1,8 @@
+from logger import log
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler
+from utils.keyboards import WEEK_KEYBOARD
+
+
+def register(dispatcher: object) -> None:
+    pass
