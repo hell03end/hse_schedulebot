d31246b2971be42176849e58792c13bd02122e37
Bogdan <evstrat.bg@mail.ru>
2017-10-20 17:19:58 +0300

    add mailing func

--- service/__init__.py
+++ service/__init__.py
@@ -1,1 +1,6 @@
-from service.common_handlers import register
+def register(dispatcher: object) -> None:
+    from service.common_handlers import register as common_handlers_reg
+    from service.mailing import register as mailing_reg
+
+    common_handlers_reg(dispatcher)
+    mailing_reg(dispatcher)
