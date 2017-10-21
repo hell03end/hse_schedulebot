1922f1ce1c8a25938b8aeb6485be4d0da3b6be1e
Alex <eshobee@gmail.com>
2017-10-22 00:17:05 +0300

    remove imports from utils.update_db to resolve cycled imports conflict

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,3 @@
-from utils.update_db import (api, get_emails, fetch_schedules, format_schedule,
-                             update_schedules)
 from utils.schema import MESSAGE_SCHEMA, POST_SCHEMA
 from utils.functions import is_cancelled
 from utils.keyboards import START_KEYBOARD, WEEK_KEYBOARD
