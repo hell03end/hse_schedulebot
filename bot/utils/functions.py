f067b3b866c363c54ecfa633e68fb28f41bb3095
Alex <eshobee@gmail.com>
2017-10-22 21:17:08 +0300

    add is_back function, update annotations

--- bot/utils/functions.py
+++ bot/utils/functions.py
@@ -1,2 +1,9 @@
-def is_cancelled(msg):
-    return True if msg.strip('/').lower() in ('back', 'start') else False
+from bot.utils.keyboards import BACK_KEY
+
+
+def is_cancelled(msg: str) -> bool:
+    return msg.strip('/').lower() in ('back', 'start')
+
+
+def is_back(msg: str) -> bool:
+    return msg == BACK_KEY[0]
