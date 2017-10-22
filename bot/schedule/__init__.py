caa1259336d119563981527ada2acf78dc04c225
Alex <eshobee@gmail.com>
2017-10-22 21:44:49 +0300

    add schedule/start.py to connect schedule with start menu

--- bot/schedule/__init__.py
+++ bot/schedule/__init__.py
@@ -4,6 +4,8 @@ from telegram.ext.dispatcher import Dispatcher
 def register(dispatcher: Dispatcher) -> None:
     from bot.schedule.week import register as week_schedule_reg
     from bot.schedule.day import register as day_schedule_reg
+    from bot.schedule.start import register as start_schedule_reg
 
     week_schedule_reg(dispatcher)
     day_schedule_reg(dispatcher)
+    start_schedule_reg(dispatcher)
---
+++ bot/schedule/start.py
@@ -0,0 +1,44 @@
+from bot.logger import log
+from bot.service.common_handlers import start
+from bot.utils.keyboards import SCHEDULE_KEYBOARD, START_KEYBOARD, BACK_KEY
+from bot.utils.messages import MESSAGES
+from telegram import ReplyKeyboardMarkup
+from telegram.bot import Bot
+from telegram.ext import CommandHandler, ConversationHandler, RegexHandler
+from telegram.ext.dispatcher import Dispatcher
+
+MESSAGES = MESSAGES['schedule:start']
+
+
+@log
+def on_schedule(bot: Bot, update: object) -> None:
+    bot.send_message(
+        update.message.from_user.id,
+        MESSAGES['on_schedule:ask'],
+        reply_markup=ReplyKeyboardMarkup(SCHEDULE_KEYBOARD)
+    )
+
+
+@log
+def on_back(bot: Bot, update: object) -> None:
+    bot.send_message(
+        update.message.from_user.id,
+        MESSAGES['on_back:msg'],
+        reply_markup=ReplyKeyboardMarkup(START_KEYBOARD)
+    )
+    return ConversationHandler.END
+
+
+def register(dispatcher: Dispatcher) -> None:
+    start_schedule = ConversationHandler(
+        entry_points=[RegexHandler(START_KEYBOARD[0][0], on_schedule)],
+        states={},
+        fallbacks=[CommandHandler('start', start)]
+    )
+    back_from_schedule = ConversationHandler(
+        entry_points=[RegexHandler(BACK_KEY[0], on_back)],
+        states={},
+        fallbacks=[CommandHandler('start', start)]
+    )
+    dispatcher.add_handler(start_schedule)
+    dispatcher.add_handler(back_from_schedule)
