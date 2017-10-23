c0eb2bbeb4f47d3134075d563e8ecad7b64f369b
Alex <eshobee@gmail.com>
2017-10-23 03:02:08 +0300

    add typing decorator

--- bot/utils/functions.py
+++ bot/utils/functions.py
@@ -1,4 +1,10 @@
+from collections import Callable
+from functools import wraps
+
+import telegram
 from bot.utils.keyboards import BACK_KEY
+from telegram.bot import Bot
+from telegram.update import Update
 
 
 def is_cancelled(msg: str) -> bool:
@@ -7,3 +13,14 @@ def is_cancelled(msg: str) -> bool:
 
 def is_back(msg: str) -> bool:
     return msg == BACK_KEY[0]
+
+
+def typing(func: Callable) -> Callable:
+    @wraps(func)
+    def decorator(bot: Bot, update: Update, *args, **kwargs) -> object:
+        bot.send_chat_action(
+            chat_id=update.message.chat.id,
+            action=telegram.ChatAction.TYPING
+        )
+        return func(bot, update, *args, **kwargs)
+    return decorator
