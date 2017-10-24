45c4c83b5828f55babdc57a2ca2d798bb49d628a
Завьялов Андрей <Aozavyalov@users.noreply.github.com>
2017-10-24 10:44:34 +0300

    Try io implement lecturers searching

--- bot/service/find_lecturer.py
+++ bot/service/find_lecturer.py
@@ -1,11 +1,32 @@
+from bot.logger import log
 from bot.models import Lecturers
+from bot.service.common_handlers import send_cancel
+from bot.utils.functions import is_back, is_cancelled, typing
+from bot.utils.keyboards import SCHEDULE_KEYBOARD_STUDENT
 
+from telegram import ParseMode, ReplyKeyboardMarkup
+from telegram.bot import Bot
+from telegram.ext import MessageHandler
+from telegram.update import Update
 
 def find(asked_fio: str) -> list:
     try:
-        query = Lecturers.select(Lecturers.lecturer_id).\
+        query = Lecturers.select().\
             where(Lecturers.fio.contains(asked_fio.lower())).\
             order_by(Lecturers.fio)
-        return [lect.lecturer_id for lect in query]
-    except Lecturers.DoesNotExist:
+        return [{'fio': lect.fio} for lect in query]
+    except Lecturers.DoesNotExist as err:
+        print(err)
         return []
+
+
+@log
+@typing
+def get_lecturers(bot: Bot, update: Update) -> None:
+    lect_name = bot.sendMessage()
+    bot.send_message(
+        update.message.from_user.id,
+        'Список найденных преподавателей. Выбери нужного:',
+        reply_markup=ReplyKeyboardMarkup(find(lect_name), True)
+    )
+    return
