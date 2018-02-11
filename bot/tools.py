2ec6739274cbf645a92860daabd50c2ba7463115
Alex <eshobee@gmail.com>
2018-02-12 00:55:57 +0300

    update bot.tools

--- bot/tools.py
+++ bot/tools.py
@@ -1,14 +1,18 @@
 import logging
 import pickle
+from threading import Thread
+from time import sleep
 
-from telegram.ext import ConversationHandler, Dispatcher
+from telegram import Bot
+from telegram.ext import (ConversationHandler, Dispatcher, Filters,
+                          MessageHandler, Updater)
 from telegram.ext.dispatcher import Dispatcher
-from config import LOGGING_LEVEL
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=LOGGING_LEVEL
-)
+from bot import schedule, service
+from bot.models.models import Lecturers, Lessons, Users
+from bot.models.tools import create_tables, drop_tables
+from bot.service.common_handlers import start
+from config import CONVERSATIONS_PATH, USERDATA_PATH
 
 
 def load_data(conv_path: str, user_data_path: str) -> None:
@@ -23,11 +27,10 @@ def load_data(conv_path: str, user_data_path: str) -> None:
         logging.error(excinfo)
 
 
-def save_data(period: (float, int), conv_path: str,
+def save_data(period: float or int,
+              conv_path: str,
               user_data_path: str) -> None:
     """ Infinite loop for with saving bot state """
-    from time import sleep
-
     while True:
         sleep(period)
         # Before pickling
@@ -52,10 +55,8 @@ def save_data(period: (float, int), conv_path: str,
             logging.error(excinfo)
 
 
-def save_state(period: (float, int)=60, **kwargs) -> None:
+def save_state(period: float or int=60, **kwargs) -> None:
     """ Start infinite state saving in separate thread """
-    from threading import Thread
-
     thread = Thread(
         name="save_data",
         args=period,
@@ -67,44 +68,24 @@ def save_state(period: (float, int)=60, **kwargs) -> None:
 
 def register(dispatcher: Dispatcher) -> None:
     """ Register all handlers with bot dispatcher """
-    from bot import buses, schedule, service, trains
-    from telegram.ext import MessageHandler, Filters
-    from bot.service.common_handlers import start
-
     service.register(dispatcher)
     schedule.register(dispatcher)
-    buses.register(dispatcher)
-    trains.register(dispatcher)
     dispatcher.add_handler(MessageHandler(Filters.text, start))
 
 
 def init_db() -> None:
     """ (re)Create tables in database """
-    from bot.models.tools import create_tables, drop_tables
-    from bot.models import TABLES
-
-    drop_tables(TABLES)
-    create_tables(TABLES)
+    drop_tables(Users, Lecturers, Lessons)
+    create_tables(Users, Lecturers, Lessons)
 
 
-def run(token: str, logger_level: int=0, workers: int=10) -> None:
+def run(token: str, workers: int=10) -> None:
     """ Start bot """
-    from config import CONVERSATIONS_PATH, USERDATA_PATH
-    from telegram import Bot
-    from telegram.ext import Updater
-    import logging
-
-    logging.basicConfig(
-        filemode='a',
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logger_level
-    )
-
     # load previous state to continue chats correctly
     # load_data(CONVERSATIONS_PATH, USERDATA_PATH)
 
     updater = Updater(token, workers=workers)
-    api = Bot(token)  # api is used to resolve name conflicts with main module
+    bot_api = Bot(token)
     register(updater.dispatcher)
 
     updater.start_polling()
